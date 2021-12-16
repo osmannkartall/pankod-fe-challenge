1ad7c3ab26db68a38077a01106b2c87c8dca3e8d
Osman Kartal <kartal.osman@outlook.com.tr>
2021-12-16 13:29:09 +0300

    add card and cards components

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,11 +1,44 @@
 import React from "react";
 import { Card as AntdCard } from "antd";
 import { CardProps } from "antd/lib/card";
+import { Typography } from 'antd';
+import { PlayCircleOutlined } from '@ant-design/icons';
+import { useRouter } from 'next/router'
+
+const { Title, Text } = Typography;
 
 export const Card: React.FC<CardProps> = ({ title, children, ...rest }) => {
+  const router = useRouter();
+
   return (
-    <AntdCard title={title} bordered={false}>
-      {children}
-    </AntdCard>
+    <>
+      <AntdCard
+        hoverable
+        style={{ width: 180, backgroundColor: "#1E1E1E"}}
+        onClick={() => router.push(`/${title}`)}
+      >
+        <div
+          style={{
+            height: 200,
+            textAlign: "center",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <PlayCircleOutlined
+            style={{
+              color: "#fff",
+              fontSize: 100,
+              opacity: .2,
+              position: "absolute",
+              zIndex: 2,
+            }}
+          />
+          <Title style={{color: "#fff", position: "absolute", zIndex: 0 }}>{title?.toString().toUpperCase()}</Title>
+        </div>
+      </AntdCard>
+      <Text>{`Popular ${title}`}</Text>
+    </>
   );
 };
--- src/components/cards/index.tsx
+++ src/components/cards/index.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import { Row, Col } from "antd";
-
 import { Card } from "@components";
-import data from "@public/meta.json";
+
+const streamItems = ["series", "movies"]
 
 export const Cards: React.FC = () => {
   return (
-    <Row style={{ flex: 1 }}>
-      {(data?.plugins ?? []).map((plugin) => (
-        <Col md={6} key={plugin.name}>
-          <Card title={plugin.name}>{plugin.description}</Card>
+    <Row
+      style={{
+        flex: 1,
+        padding: "40px 200px",
+      }}
+    >
+      {(streamItems ?? []).map((streamItem) => (
+        <Col md={4} key={streamItem}>
+          <Card title={streamItem} />
         </Col>
       ))}
     </Row>
