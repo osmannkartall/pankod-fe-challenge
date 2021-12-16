e079e9dd12e14d0ee896232ac7327142bdd2af94
Osman Kartal <kartal.osman@outlook.com.tr>
2021-12-16 20:13:58 +0300

    create footer component

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,62 +1,143 @@
 import React, { CSSProperties } from "react";
-import Image from "next/image";
-import { Space } from "antd";
 import {
-  GithubOutlined,
   TwitterOutlined,
-  YoutubeOutlined,
-  LinkedinOutlined,
+  FacebookOutlined,
+  InstagramOutlined,
 } from "@ant-design/icons";
 
-export const Footer: React.FC = () => {
+const footerLinks = [
+  "Home",
+  "Terms and Conditions",
+  "Privacy Policy",
+  "Collection Statement",
+  "Help",
+  "Manage Account"
+];
+
+const FooterLinks: React.FC = () => {
+  return (
+    <div style={{ opacity: 0.6 }}>
+      {
+        (footerLinks ?? []).map((footerLink, index) => (
+          <div style={{ display: "inline-block" }} key={footerLink}>
+            <a
+              style={{ textDecoration: "none", color: "#fff" }}
+              href="#"
+            >
+              {footerLink}
+            </a>
+            {
+              index !== footerLinks?.length - 1 && <span style={{ padding: "0px 12px", }}>|</span>
+            }
+          </div>
+        ))
+      }
+    </div>
+  );
+}
+
+const CopyRightText: React.FC = () => (
+  <div style={{ opacity: 0.6, marginTop: 10 }}>
+    Copyright © 2016 DEMO Streaming All Right Reserved.
+  </div>
+);
+
+const StoreLogoWrapper: React.FC = () => (
+  <div
+    style={{
+      flex: "0 1 auto",
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+      backgroundColor: "red",
+      height: 100
+    }}
+  >
+    <a href="#" style={{ marginRight: 20 }}>
+      <img
+        src="/store/app-store.svg"
+        alt="pankod-app-store.svg"
+        width="135"
+        height="40"
+      />
+    </a>
+    <a href="#" style={{ marginRight: 20 }}>
+      <img
+        src="/store/play-store.svg"
+        alt="pankod-play-store.svg"
+        width="135"
+        height="40"
+      />
+    </a>
+    <a href="#">
+      <img
+        src="/store/windows-store.svg"
+        alt="pankod-windows-store.svg"
+        width="110"
+        height="40"
+      />
+    </a>
+  </div>
+)
+
+const SocialLogoWrapper: React.FC = () => {
   const iconStyle: CSSProperties = {
-    fontSize: 22,
+    fontSize: 35,
     color: "#fff",
+    marginRight: 20,
   };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        backgroundColor: "green",
+        height: 100
+      }}
+    >
+      <a
+        href="https://twitter.com/PankodDev"
+        target="_blank"
+        style={iconStyle}
+      >
+        <FacebookOutlined />
+      </a>
+      <a
+        href="https://twitter.com/PankodDev"
+        target="_blank"
+        style={iconStyle}
+      >
+        <TwitterOutlined />
+      </a>
+      <a
+        href="https://twitter.com/PankodDev"
+        target="_blank"
+        style={iconStyle}
+      >
+        <InstagramOutlined />
+      </a>
+    </div>
+  );
+};
+
+export const Footer: React.FC = () => {
   return (
     <div
       style={{
         backgroundColor: "#1E1E1E",
         color: "#fff",
-        textAlign: "center",
-        paddingTop: 32,
-        paddingBottom: 32,
+        padding: "32px 200px",
+        fontSize: 13,
       }}
     >
-      <Space direction="vertical" size="large">
-        <Image
-          src="/icons/pankod-icon.svg"
-          alt="pankod"
-          width="140"
-          height="28"
-        />
-        <Space align="center" size="middle">
-          <a href="https://github.com/pankod" target="_blank" style={iconStyle}>
-            <GithubOutlined />
-          </a>
-          <a
-            href="https://twitter.com/PankodDev"
-            target="_blank"
-            style={iconStyle}
-          >
-            <TwitterOutlined />
-          </a>
-          <a
-            href="https://www.youtube.com/channel/UCBGOeQkv1XW3ptryLWlQbAQ"
-            target="_blank"
-            style={iconStyle}
-          >
-            <YoutubeOutlined />
-          </a>
-          <a
-            href="https://www.linkedin.com/company/pankod-yazilim-ve-danismanlik/"
-            target="_blank"
-            style={iconStyle}
-          >
-            <LinkedinOutlined />
-          </a>
-        </Space>
-      </Space>
+      <FooterLinks />
+      <CopyRightText />
+      <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+        <SocialLogoWrapper />
+        <StoreLogoWrapper />
+      </div>
     </div>
   );
 };
