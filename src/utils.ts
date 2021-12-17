619f60e0be2035f20509d82486a7232f56156e85
Osman Kartal <kartal.osman@outlook.com.tr>
2021-12-17 15:50:26 +0300

    add other filtering and sort the results

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,10 @@
 import { Program } from "src/models/Program";
 
-// TODO: fix this type error
+// TODO: fix this type error, separate the releaseYear logic
 export const filterByAttribute = (items: Program[], attributeKey: string, attributeValue: string) => {
-  return items.filter((item: Program) => item[attributeKey] === attributeValue);
-}
+  const result = items.filter(
+    (item: Program) => item[attributeKey] === attributeValue && item.releaseYear >= 2010
+  );
+
+  return result.sort((a, b) => a.title.localeCompare(b.title));
+};
