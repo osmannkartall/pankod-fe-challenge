1b8cb22e422f4839e12518b80822272c8a464c71
Osman Kartal <kartal.osman@outlook.com.tr>
2021-12-21 22:25:34 +0300

    check whether array is empty before accessing it

--- src/utils.ts
+++ src/utils.ts
@@ -5,15 +5,17 @@ interface Item {
 }
 
 export function sortByAttribute(items: Item[], filterAttribute: string, sortOrder: SortOrder) {
-  const filterAttributeValueType = typeof items[0][filterAttribute];
+  if (items.length) {
+    const filterAttributeValueType = typeof items[0][filterAttribute];
 
-  if (filterAttributeValueType === "string" && sortOrder === "asc") {
-    items.sort((a: Item, b: Item) => a[filterAttribute].localeCompare(b[filterAttribute]));
-  } else if (filterAttributeValueType === "string" && sortOrder === "desc") {
-    items.sort((a: Item, b: Item) => b[filterAttribute].localeCompare(a[filterAttribute]));
-  } else if (filterAttributeValueType === "number" && sortOrder === "asc") {
-    items.sort((a: Item, b: Item) => a[filterAttribute] - b[filterAttribute]);
-  } else if (filterAttributeValueType === "number" && sortOrder === "desc") {
-    items.sort((a: Item, b: Item) => b[filterAttribute] - a[filterAttribute]);
+    if (filterAttributeValueType === "string" && sortOrder === "asc") {
+      items.sort((a: Item, b: Item) => a[filterAttribute].localeCompare(b[filterAttribute]));
+    } else if (filterAttributeValueType === "string" && sortOrder === "desc") {
+      items.sort((a: Item, b: Item) => b[filterAttribute].localeCompare(a[filterAttribute]));
+    } else if (filterAttributeValueType === "number" && sortOrder === "asc") {
+      items.sort((a: Item, b: Item) => a[filterAttribute] - b[filterAttribute]);
+    } else if (filterAttributeValueType === "number" && sortOrder === "desc") {
+      items.sort((a: Item, b: Item) => b[filterAttribute] - a[filterAttribute]);
+    }
   }
 }
