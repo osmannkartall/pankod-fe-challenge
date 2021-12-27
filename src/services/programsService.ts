0967f7ac673a8e589ede2c7239582dc653fbd8f8
Osman Kartal <kartal.osman@outlook.com.tr>
2021-12-27 15:25:46 +0300

    fix fetching of programs

--- src/services/programsService.ts
+++ src/services/programsService.ts
@@ -1,19 +1,11 @@
 import { Program } from "@models";
 
 export class ProgramsService {
-  private readonly headers = { 
-    'Content-Type': 'application/json',
-    'Accept': 'application/json'
-  };
-
-  private readonly programsPath = 'mock/sample.json';
-
-  public getAllPrograms = async () => {
+  public getAllPrograms = () => {
     try {
-      const response = await fetch(this.programsPath, { headers: this.headers })
-      const programs = await response.json();
+      const response = require('../../public/mock/sample.json');
   
-      return programs.entries.filter((program: Program) => program.releaseYear >= 2010);
+      return response.entries.filter((program: Program) => program.releaseYear >= 2010);
     } catch (err) {
       console.error(new Date().toLocaleString(), err);
     }
