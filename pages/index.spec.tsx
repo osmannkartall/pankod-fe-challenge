9224a5fd32f18afafb6931af0560dbb87c180aff
Osman Kartal <kartal.osman@outlook.com.tr>
2021-12-27 18:00:53 +0300

    add tests for home page

---
+++ pages/index.spec.tsx
@@ -0,0 +1,48 @@
+import { mount, ReactWrapper } from "enzyme"
+import React from "react";
+import Home from ".";
+import { TileCards, PageWrapper, TileCard } from "@components";
+import Title from "antd/lib/typography/Title";
+
+describe(`Testing Home Page...`, () => {
+  let home: ReactWrapper;
+
+  beforeEach(async () => {
+    home = mount(<Home />);
+  });
+
+  it("renders without crashing", () => {
+    expect(home).toBeTruthy();
+  });
+
+  it("renders a PageWrapper component", () => {
+    const pageWrapper = home.find(PageWrapper);
+
+    expect(pageWrapper).toHaveLength(1);
+  });
+
+  it("renders a PageWrapper component that have a TileCards component", () => {
+    const tileCards = home.find(PageWrapper).find(TileCards);
+
+    expect(tileCards).toHaveLength(1);
+  });
+
+  it("renders two TileCard component", () => {
+    expect(home.find(TileCard)).toHaveLength(2);
+  });
+
+  it(`renders a TileCard component with "MOVIES" title`, () => {
+    const tileCards = home.find(TileCard);
+    const titles = tileCards.find(Title).filterWhere(title => title.text().includes("MOVIES"));
+
+    expect(titles).toHaveLength(1);
+  });
+
+  it(`renders a TileCard component with "SERIES" title`, () => {
+    const tileCards = home.find(TileCard);
+    const titles = tileCards.find(Title).filterWhere(title => title.text().includes("SERIES"));
+
+    expect(titles).toHaveLength(1);
+  });
+});
+
