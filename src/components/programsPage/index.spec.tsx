fede4f6749ece538404b63fcf6be732bf7e8a7a1
Osman Kartal <kartal.osman@outlook.com.tr>
2021-12-27 15:29:28 +0300

    add more tests for ProgramsPage component

--- src/components/programsPage/index.spec.tsx
+++ src/components/programsPage/index.spec.tsx
@@ -1,16 +1,79 @@
-import { mount } from "enzyme"
+import { mount, ReactWrapper } from "enzyme"
+import React from "react";
 import ProgramsPage from "."
+import { FilterWrapper, ProgramCard, ProgramCards, Searchbar } from "..";
+import { act } from "react-dom/test-utils";
+import Home from "../../../pages";
 
-describe("Testing ProgramsPage component...", () => {
-  it("renders without crashing with programType=\"movie\"", () => {
-    const component = mount(<ProgramsPage programType="movie" />);
-
-    expect(component).toBeTruthy();
+const simulateAsyncEvent = async (component: ReactWrapper, ms: number = 0) => {
+  await act(async () => {
+    await new Promise(resolve => setTimeout(resolve, ms));
+    component.update();
   });
+};
 
-  it("renders without crashing with programType=\"series\"", () => {
-    const component = mount(<ProgramsPage programType="series" />);
-
-    expect(component).toBeTruthy();
+const runDescribeBlock = (programType: "series" | "movie") => {
+  describe(`Testing ProgramsPage component...`, () => {
+    let home: ReactWrapper;
+    let programsPage: ReactWrapper;
+  
+    beforeEach(async () => {
+      home = mount(<Home />);
+      await simulateAsyncEvent(home);
+  
+      programsPage = mount(<ProgramsPage programType={programType} />);
+      await simulateAsyncEvent(programsPage, 100);
+    });
+  
+    it(`renders without crashing when programType="${programType}"`, () => {
+      expect(programsPage).toBeTruthy();
+    });
+  
+    it(`renders an FilterWrapper component that have a Searchbar component when programType="${programType}"`, () => {
+      const filterWrapperComponent = programsPage.find(FilterWrapper);
+  
+      expect(filterWrapperComponent).toHaveLength(1);
+      expect(filterWrapperComponent.find(Searchbar)).toHaveLength(1);
+    });
+  
+    it(`renders ${programType} cards or "No program found" text when programType="${programType}"`, async () => {
+      const hasProgramCards = programsPage.contains(<ProgramCards />);
+      const noProgramFound = programsPage.contains("No program found...");
+  
+      expect(hasProgramCards !== noProgramFound).toBe(true);
+    });
+  
+    it(`renders no "Load More" button if there is no ProgramCards, otherwise it renders 0 or 1 "Load More" button when programType="${programType}"`, async () => {
+      const loadMoreButton = programsPage.findWhere(node => {
+        return node.type() === 'button' && node.text() === "Load More";
+      });
+  
+      if (!programsPage.contains(<ProgramCards />)) {
+        expect(loadMoreButton).toHaveLength(0);
+      } else {
+        expect(loadMoreButton.length === 1 || loadMoreButton.length === 0).toBe(true);
+      }
+    });
+  
+    it(`renders more cards after clicking "Load More" button when programType="${programType}"`, async () => {
+      if (programsPage.contains(<ProgramCards />)) {
+        const numProgramCardBefore = programsPage.find(ProgramCard).length;
+        const loadMoreButton = programsPage.findWhere(node => {
+          return node.type() === 'button' && node.text() === "Load More";
+        });
+  
+        if (loadMoreButton.length === 1) {
+          loadMoreButton.simulate("click");
+          await simulateAsyncEvent(programsPage, 100);
+    
+          const numProgramCardAfter = programsPage.find(ProgramCard).length;
+          
+          expect(numProgramCardAfter).toBeGreaterThan(numProgramCardBefore);
+        }
+      }
+    });
   });
-})
+}
+
+runDescribeBlock("movie");
+runDescribeBlock("series");
